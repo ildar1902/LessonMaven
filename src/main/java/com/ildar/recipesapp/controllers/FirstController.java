60b68b847f3fdfa5fb9ce03b0bb7446fb005b2a6
Home <ildar86@live.ru>
2022-12-27 20:36:36 +0500

    дз выполнено

--- src/main/java/com/ildar/recipesapp/controllers/FirstController.java
+++ src/main/java/com/ildar/recipesapp/controllers/FirstController.java
@@ -1,8 +1,19 @@
 package com.ildar.recipesapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping("/")
+    public String hello() {
+        return "Приложение запущено!";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "Имя студента, название проекта, дата создания проекта, описание проекта в свободной форме" ;
+    }
 
 }
