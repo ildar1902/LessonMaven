819c273e5ce4e53c1b7e5aa62224db2cbd9549c3
Home <ildar86@live.ru>
2022-12-27 19:36:54 +0500

    first commit

---
+++ src/test/java/com/ildar/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ildar.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
